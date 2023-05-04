9ffede313a57dfbe6ffd45f04a9d334a28b787c4
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-05-03 23:30:09 -0400

    add database schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department(id, name)
+VALUES 
+('001','Sales'),
+('002','Engineering'),
+('003','Finance'),
+('004','Legal')
+;
+
+INSERT INTO role(id,title,salary,department_id)
+VALUES
+(001,"Sales Lead",100000,001),
+(002,"Lead Engineer",110000,002),
+(003,"Accountant",120000,003),
+(004,"Lawyer",100000,004)
+;
+
+INSERT INTO employee(id,first_name,last_name,role_id,manager_id)
+VALUES
+(001,'John','Doe',001,001),
+(002,'John','Smith',002,001),
+(003,'John','Jones',003,001),
+(004,'John','Tall',004,0041)
+;
